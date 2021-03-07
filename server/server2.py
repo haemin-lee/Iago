d628a66699736fac52db5365a24ab06163577853
Anjali Gopinathan <asgopina@usc.edu>
2021-03-07 03:45:33 -0800

    aaaaaaaaaa

--- server/server2.py
+++ server/server2.py
@@ -3,37 +3,39 @@ from flask import request
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 import json
+from sqlalchemy.dialects import postgresql, mysql, sqlite # import uuid
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buttons.db'
 
 db = SQLAlchemy(app)
-
+db.drop_all()
 class Buttons(db.Model):
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     color = db.Column(db.String(20), nullable=False)
 
-new_button = Buttons(id="button", color="Black")
+new_button = Buttons(color="Black")
+db.create_all()
 
-try:
-    db.session.add(new_button)
-    db.session.commit()
-except:
-    print("There was error adding the first element of db")
-
+# try:
+db.session.add(new_button)
+db.session.commit()
+# except Exception as e:
+    # print("There was error adding the first element of db")
+    # print("error?idk\n", e)
 @app.route('/',methods = ['POST', 'GET'])
 def result():
     print("Entered result()")
 
     if request.method == 'POST':
         content = request.get_json(force=True)
-        thiscolor = content['button']
+        thiscolor = content["Button"]
 
         x = db.session.query(Buttons).get("button")
         x.color = thiscolor
         db.session.commit()
 
-        return str(content)
+        return jsonify(content)
 
     if request.method == 'GET':
         x = db.session.query(Buttons).first()
