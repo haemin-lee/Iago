6e7ab56c7ce9d247a834f9a5de8cc0fe54917bca
emilylondon <elondon@usc.edu>
2021-03-06 21:31:21 -0800

    Flask client with buttons

--- device/piCode/IagoModule.py
+++ device/piCode/IagoModule.py
@@ -1,16 +1,47 @@
+from pprint import pprint
+
 from gpiozero import Button 
 from time import sleep
+from flask import flask
+import requests 
+import json 
+
+class iagoClient():
+    def __init__(self, addr):
+        self.serv_addr = addr
+
+    def send_press(self, color, addr):
+        #post request to post the button press
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': None
+        }
+        payload = {
+            'Button': color
+        }
+
+        #Send an HTTP POST message and block until a respone 
+        response = requests.post("http://{}/send-mail".format(addr), 
+                                headers=headers,
+                                data=json.dumps(payload))
+        pprint(response.json())
+
+
 XButton = Button (1)
 OButton = Button (7)
 TButton = Button (8)
 SButton = Button (25)
-while True:
-    if  XButton.is_pressed:
-        print("XButton Pressed")
-    elif OButton.is_pressed:
-        print("OButton Pressed")
-    elif TButton.is_pressed:
-        print("TButton Pressed")
-    elif SButton.is_pressed:
-        print("SButton Pressed")
-    sleep(1)
+address='162.208.92.132'
+
+def main():
+    buttonClient = iagoClient(address)
+    while True:
+        if  XButton.is_pressed:
+            buttonClient.send_press('blue', address)
+        elif OButton.is_pressed:
+            buttonClient.send_press('yellow', address)
+        elif TButton.is_pressed:
+            buttonClient.send_press('green', address)
+        elif SButton.is_pressed:
+            buttonClient.send_press('red', address)
+        sleep(0.5)
