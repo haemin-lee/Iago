d031474ce0996b62c4e11e6f53bf34f02d42dbe6
Anjali Gopinathan <asgopina@usc.edu>
2021-03-07 04:15:25 -0800

    removing old server files cuz i was lazy and didn't do this the correct git way

--- server/server.py
+++ server/server.py
@@ -1,40 +1,38 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask import g
 import json
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def index():
-#     headers = request.headers
-#     return "Request headers:\n" + str(headers)
+# def get_color():
+#     if 'color' not in g:
+#         g.color = "black"
+#     return g.color
+color = "black"
 
-# content = jsonify({"hello" : "world"})
 @app.route('/',methods = ['POST', 'GET'])
 def result():
+    global color
     print("Entered result()")
-    content = "default"
 
-    # result = "oopsie"
     if request.method == 'POST':
-        # result = request.form
-        # headers = request.headers
         content = request.get_json(force=True)
-        # print()
-        # return "Request\n\nresult:\n" + str(result) + "\nheaders:\n" + str(headers) + "\ncontent:\n" + content
-        print("this is the content being posted:\n", jsonify(content))
+        # with app.app_context(): 
+        color = content["Button"]
+        # g['color'] = content["Button"]
+        print("content[Button] = " + content["Button"], "\n\tg.color = ", color)
+
         return jsonify(content)
 
     if request.method == 'GET':
-        # result = content
-        # content = request.args.get('Button')
-        # content = request.json['Button']
-        content = request.json
-        # content = request.get_json(force=True)
-        # return "Request<br>result:<br>" + "<br>content:<br><br>" + str(content)
-        print("okay now this is the content that was gotten:\n", jsonify(content))
-        return jsonify(content)
+        # with app.app_context(): 
+        c = color
+        # c = g.color
+        print("got color: " + c)
+        return c
+
 
 
 if __name__ == "_main_":
