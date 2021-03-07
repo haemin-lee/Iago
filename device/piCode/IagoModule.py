f4f6e88b2ba86da05698330cedc27d12c965f679
emilylondon <elondon@usc.edu>
2021-03-06 21:57:19 -0800

    Print statements for debugging

--- device/piCode/IagoModule.py
+++ device/piCode/IagoModule.py
@@ -31,17 +31,21 @@ XButton = Button (1)
 OButton = Button (7)
 TButton = Button (8)
 SButton = Button (25)
-address='162.208.92.132:5000'
+address='localhost:5000'
 
 
 buttonClient = iagoClient(address)
 while True:
     if  XButton.is_pressed:
         buttonClient.send_press('blue', address)
+        print("blue")
     elif OButton.is_pressed:
         buttonClient.send_press('yellow', address)
+        print("yellow")
     elif TButton.is_pressed:
         buttonClient.send_press('green', address)
+        print("green")
     elif SButton.is_pressed:
         buttonClient.send_press('red', address)
+        print("red")
     sleep(0.5)
